d6d14f3fa329c10191ca059641d0a5bfb5244de6
Yifeng Jiang <uprushworld@gmail.com>
2014-05-29 21:34:21 +0900

    use remote_file instead of curl

--- wp-cli/recipes/default.rb
+++ wp-cli/recipes/default.rb
@@ -8,9 +8,13 @@ package "curl" do
    action :install
 end
 
+remote_file "#{Chef::Config[:file_cache_path]}/wp-cli.phar" do
+  source "https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+end
+
 execute "wp-cli install" do
-   command "curl -L https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > wp-cli.phar && chmod +x wp-cli.phar && cp wp-cli.phar #{wpdir}/wp"
-   cwd '/home/ec2-user'
+   command "cp #{Chef::Config[:file_cache_path]}/wp-cli.phar #{wpdir}/wp && chmod +x #{wpdir}/wp"
+   cwd "#{Chef::Config[:file_cache_path]}"
    action :run
    not_if { ::File.exists?("#{wpdir}/wp")}
 end
