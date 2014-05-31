e2a6548b1409a1c44bd4f4f143e8790d9d9d2a17
Yifeng Jiang <uprushworld@gmail.com>
2014-05-31 10:12:11 +0900

    remove unnecessary packages

--- wp-cli/recipes/default.rb
+++ wp-cli/recipes/default.rb
@@ -1,13 +1,5 @@
 wpdir = "/usr/bin/"
 
-package "mysql" do
-   action :install
-end
-
-package "curl" do
-   action :install
-end
-
 remote_file "#{wpdir}/wp" do
   source "https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
   owner "root"
