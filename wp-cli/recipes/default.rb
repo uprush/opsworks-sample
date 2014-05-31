13db7910b8385e980ef906c43bb525d09b4dd1d3
Yifeng Jiang <uprushworld@gmail.com>
2014-05-31 17:44:15 +0900

    bug fix

--- wp-cli/recipes/default.rb
+++ wp-cli/recipes/default.rb
@@ -1,4 +1,23 @@
+# wpdir = "/srv/www/wordpress/current"
+
+# install git
+# package 'git' do
+#   action :install
+# end
+
+# download wordpress
+# git "#{wpdir}/" do
+#   repository "github.com/WordPress/WordPress.git"
+#   revision "master"
+#   action :sync
+# end
+
+
 wpdir = "/usr/bin/"
+
+package 'mysql' do
+  action :install
+end
 
 remote_file "#{wpdir}/wp" do
   source "https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
